807c61b3597bf1446fe14e3a78c2b0a7fc89eb66
Amit Hetawal <amit.hetawal@gmail.com>
2016-06-23 11:38:36 -0700

    Try this

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,29 @@ var templateUtil = require('../util/ejsutil.js');
 var emailUtil = require('../util/emailutil.js');
 var dbUtil = require('../util/dbutil.js');
 var url  = require('url');
+var twilio = require("twilio");
+
+var accountSid = 'AC95fe367f44081f3dd847deefde865a44'; // Your Account SID from www.twilio.com/console
+var authToken = '81db504afcaa33b2adfb53de52e9463a';   // Your Auth Token from www.twilio.com/console
+var twilio = require('twilio');
+var client = new twilio.RestClient(accountSid, authToken);
 
 
 var router = express.Router();
+
+/* GET home page. */
+router.get('/sendsms', function(req, res, next) {
+	client.messages.create({
+    body: 'Hello from Salesforce',
+    to: '+14156402834',  // Text this number
+    from: '+16507535865' // From a valid Twilio number
+	},
+	function(err, message) {
+    	console.log(message.sid);
+    	res.send("Sent");
+	});
+});
+
 
 
 /* GET home page. */
